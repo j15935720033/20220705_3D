68111549791b2f6b10154d3f08e48727ea71c9cc
j15935720033 <99215381@ulive.pccu.edu.tw>
2022-08-08 16:31:20 +0800

    update

--- 20220705_3D/Assets/Script/DialogueSystem.cs
+++ 20220705_3D/Assets/Script/DialogueSystem.cs
@@ -13,6 +13,9 @@ namespace chia
     [RequireComponent(typeof(AudioSource))]
     public class DialogueSystem : MonoBehaviour
     {
+        //委派簽名，無傳回與無參數
+        public delegate void DelegateFinishDialogue();
+
         [SerializeField, Header("畫布對話系統")]
         private CanvasGroup groupDialogue;
 
@@ -27,53 +30,92 @@ namespace chia
 
         private AudioSource aud;
         public DataNPC dataNpc;
+        [SerializeField, Header("淡入時間")]
+        private float intervalFadIn = 0.1f;
+        [SerializeField, Header("打字間隔")]
+        private float intervalType = 0.05f;
 
+        
 
-
-
-
+        #region Unity方法
         private void Awake()
         {
             aud = GetComponent<AudioSource>();
-            StartCoroutine(FadeIn());//啟動偕同程序
+            //StartCoroutine(Fade());//啟動偕同程序
+            //StartCoroutine(StartDialogue());//啟動偕同程序_對話欄測試
 
+        }
+        #endregion
+
+        #region 公開資料與方法
+        public bool isDialogue;//是否在對話中
+
+        public IEnumerator StartDialogue(DataNPC _dataNPC,DelegateFinishDialogue callback)
+        {
+            isDialogue = true;//設定對話中
+
+            dataNpc = _dataNPC;
             textName.text = dataNpc.nameNPC;//
             textContent.text = "";//清空對話欄
+            yield return StartCoroutine(Fade());//淡入效果    yield return跑完這行才會跑下一行
+
+            for (int i = 0; i < dataNpc.dataDialogue.Length; i++)
+            {
+                yield return StartCoroutine(TypeEffect(i));//淡入文字
+
+                while (!Input.GetKeyDown(KeyCode.E)) //如果還沒按 指定按鍵(E) 就持續等待
+                {
+                    yield return null;//傳回null，是等待1個影隔時間(1/60秒)
+                }
+            }
+
+            StartCoroutine(Fade(false));//淡出效果
+
+            isDialogue = false;//設定對話結束
+
+            callback();//執行回呼涵式
         }
+        #endregion
 
         /// <summary>
         /// 淡入效果
         /// </summary>
-        private IEnumerator FadeIn()
+        private IEnumerator Fade(bool fadeIn=true)//給預設值
         {
-            for(int i = 0; i < 10; i++)
+            //三元運算子
+            //布林直?布林值為 true:布林值為 false
+            float increase=fadeIn?0.1f:-0.1f;
+
+            for (int i = 0; i < 10; i++)
             {
-                groupDialogue.alpha += 0.1f;
-                yield return new WaitForSeconds(0.1f);
+                groupDialogue.alpha += increase;
+                yield return new WaitForSeconds(intervalFadIn);
             }
-            StartCoroutine(TypeEffect());//淡入文字
+            //StartCoroutine(TypeEffect());//淡入文字
         }
         /// <summary>
-        /// 打字淡入效果
+        /// 打字淡入效果、播放對話音效、顯示三角形
         /// </summary>
-        private IEnumerator TypeEffect()
+        private IEnumerator TypeEffect(int indexDialogue)
         {
-            aud.PlayOneShot(dataNpc.dataDialogue[0].sound);//播聲音
-            string content = dataNpc.dataDialogue[0].content;
-            for (int i=0;i<content.Length;i++)
+            textContent.text = "";//清空對話欄
+            aud.PlayOneShot(dataNpc.dataDialogue[indexDialogue].sound);//播放對話音效
+            string content = dataNpc.dataDialogue[indexDialogue].content;
+            for (int i=0;i<content.Length;i++)//打字淡入效果
             {
                 textContent.text += content[i];
-                yield return new WaitForSeconds(0.05f);
+                yield return new WaitForSeconds(intervalType);
             }
-            goTriangle.SetActive(true);
+            goTriangle.SetActive(true);//顯示三角形
         }
-        #region 偕同程序教學
 
-        
+
+
+        #region 偕同程序教學
         private IEnumerator Test()
         {
             print("第一行文字");
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(2);//等2秒
             print("第二行文字");
         }
         #endregion
---
+++ 20220705_3D/Assets/Script/NPCSystem.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace chia
+{
+    /// <summary>
+    /// 偵測玩家是否走進區域內,顯示提示畫面,按鍵偵測並啟動對話系統
+    /// </summary>
+    public class NPCSystem : MonoBehaviour
+    {
+       
+
+        [SerializeField, Header("NPC 對話系統")]
+        private DataNPC dataNpc;
+        [SerializeField, Header("NPC 攝影機")]
+        private GameObject goCamera;
+
+        private Animator aniTip;
+        private string parTipFad= "Trigger_提示淡出淡入";
+        private bool isInTrigger;//玩家是否走進對話碰撞區域內
+        private ThirdPersonController thirdPersonController;//玩家移動
+        private DialogueSystem dialogueSystem;
+        private Animator ani;
+        private string parDialogue = "對話開關";
+        private test[] t;
+        private void Awake()
+        {
+            aniTip = GameObject.Find("Image_提示底圖").GetComponent<Animator>();
+            thirdPersonController = FindObjectOfType<ThirdPersonController>();//FindObjectOfType:搜尋元件，僅限只有一個GameObjet有這個元件
+            //t = FindObjectsOfType<test>();
+            dialogueSystem = FindObjectOfType<DialogueSystem>();//找Script
+            ani = GetComponent<Animator>();
+        }
+        private void Update()
+        {
+            InputKeyAndStarDialogue();
+        }
+
+        //碰撞事件
+        //1.兩個物件至少有一個 Rigidbody
+        //2.有勾選 Trigger 使用OnTrigger 事件:[Enter、Exit、stay]
+        private void OnTriggerEnter(Collider other)
+        {
+            //print("進入偵測區域"+other.name);
+            CheckPlayerAndAnimation(other.name,true);
+
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            //print("離開偵測區域" + other.name);
+            CheckPlayerAndAnimation(other.name,false);
+        }
+
+        /// <summary>
+        /// 檢查玩家是否進入或離開並更新動畫
+        /// </summary>
+        /// <param name="nameHit">碰撞物件名稱</param>
+        private void CheckPlayerAndAnimation(string nameHit,bool isInTrigger)
+        {
+            if (nameHit=="骨頭先生")
+            {
+                this.isInTrigger = isInTrigger;
+                aniTip.SetTrigger(parTipFad);
+            }
+        }
+        /// <summary>
+        /// 對話中按E對話
+        /// </summary>
+        private void InputKeyAndStarDialogue()
+        {
+            if (this.isInTrigger && Input.GetKeyDown(KeyCode.E))
+            {
+                //print("按下 E 開始對話");
+
+                
+                if (dialogueSystem.isDialogue)//對話中，就return 
+                {
+                    return;
+                }
+                goCamera.SetActive(true);
+                aniTip.SetTrigger(parTipFad);
+                thirdPersonController.enabled = false;//關閉此元件。禁止移動
+                try
+                {
+                    ani.SetBool(parDialogue, true);//開啟對話動畫
+                }
+                catch(System.Exception)
+                {
+                    print("<Color=#993311>缺少元件錯誤，NPC沒有 Animation</color>");
+                    //throw
+                }
+                StartCoroutine(dialogueSystem.StartDialogue(dataNpc, ResetControllerAndCloseCamera));//StartDialogue方法是IEnumerator 所以要用StartCoroutine 啟動偕同程序
+            }
+        }
+        /// <summary>
+        /// 重新設定控制器與關閉攝影機
+        /// </summary>
+        private void ResetControllerAndCloseCamera()
+        {
+            goCamera.SetActive(false);
+            thirdPersonController.enabled = true;
+            aniTip.SetTrigger(parTipFad);
+            
+            try
+            {
+                ani.SetBool(parDialogue, false);//關閉對話動畫
+            }
+            catch (System.Exception)
+            {
+                print("<Color=#993311>缺少元件錯誤，NPC沒有 Animation</color>");
+                //throw
+            }
+        }
+    }
+}
+
